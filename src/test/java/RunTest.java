ec9a84017b3fe899be1d8f6b1bc61b9435f38d99
anmoldips14 <dipsdivate@gmail.com>
2020-11-16 09:57:24 +0530

    added changes

--- src/test/java/RunTest.java
+++ src/test/java/RunTest.java
@@ -5,13 +5,14 @@ public class RunTest {
     @Test
     public void t_01_test_case_is_failed(){
         System.out.println("This test case is supposed to be failed.");
-        Assert.assertEquals("Test assertion is failed", true,false);
+        Assert.assertEquals("Test assertion is failed", true,true);
+
     }
 
     @Test
     public void t_02_test_case_is_passed(){
         System.out.println("This test case is supposed to be passed.");
-        Assert.assertEquals("Test assertion is passed", true,true);
+        Assert.assertEquals("Test assertion is passed", true,false);
     }
 }
 
